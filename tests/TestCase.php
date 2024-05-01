1b2f81a84a231bff5dc99005445a85a9fedc253a
Gabriel Lau <gabrielslau@gmail.com>
2024-05-01 04:28:28 -0300

    feat: added support to Laravel 11

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,19 +5,21 @@ namespace Mtvs\EloquentHashids\Tests;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Vinkla\Hashids\HashidsServiceProvider;
 
-class TestCase extends Orchestra 
+class TestCase extends Orchestra
 {
-	protected function setUp(): void
+    protected function setUp() : void
     {
         parent::setUp();
 
         $this->withoutExceptionHandling();
 
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
-        $this->withFactories(__DIR__.'/database/factories');
+        $this->withFactories(__DIR__ . '/database/factories');
 
         $this->app['config']->set('hashids', require 'config/hashids.php');
+
+        $this->artisan('migrate');
     }
 
     protected function getPackageProviders($app)
